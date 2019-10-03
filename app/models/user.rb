3ef8ef76df77dd98b7ec3e87962c043df651314f
Francisco Abalan <francisco.abalan@pjchile.com>
2019-10-03 11:42:44 -0300

    devise Fix rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :trackable, :lockable, :timeoutable and :omniauthable
   extend Devise::Models
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
-
 end
