9aa91adfeec10d431a637b70ba4b2a5b824898cf
Francisco Abalan <francisco.abalan@pjchile.com>
2019-10-03 14:18:16 -0300

    devise validate presence names and remove unnecessaries attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191003132243) do
+ActiveRecord::Schema.define(version: 20191003171359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "gender", null: false
+    t.string "author", null: false
+    t.string "image", null: false
+    t.string "title", null: false
+    t.string "publisher", null: false
+    t.string "year", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -23,9 +34,6 @@ ActiveRecord::Schema.define(version: 20191003132243) do
     t.datetime "reset_password_sent_at"
     t.boolean "allow_password_change", default: false
     t.datetime "remember_created_at"
-    t.string "name"
-    t.string "nickname"
-    t.string "image"
     t.string "email"
     t.string "first_name", null: false
     t.string "last_name", null: false
