4c1ffa5dc3c39612a9d40f357dc484d77722c25d
Francisco Abalan <francisco.abalan@pjchile.com>
2019-10-07 11:50:13 -0300

    devise fix pair revision comments 2

--- db/schema.rb
+++ db/schema.rb
@@ -15,17 +15,6 @@ ActiveRecord::Schema.define(version: 20191003171359) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: :cascade do |t|
-    t.string "gender", null: false
-    t.string "author", null: false
-    t.string "image", null: false
-    t.string "title", null: false
-    t.string "publisher", null: false
-    t.string "year", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
     t.string "uid", default: "", null: false
