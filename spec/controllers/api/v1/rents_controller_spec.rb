da191b967e999cc7d02c5effb37cd706b0522417
Francisco Abalan <francisco.abalan@pjchile.com>
2019-10-17 13:33:13 -0300

    rent2 rspec to index and create WIP 1

--- spec/controllers/api/v1/rents_controller_spec.rb
+++ spec/controllers/api/v1/rents_controller_spec.rb
@@ -1,75 +1,73 @@
 require 'rails_helper'
 
-describe Api::V1::RentsController, type: :controller do
+describe Api::V1::RentsController do
   include_context 'with Authenticated User'
 
   describe 'GET #index' do
-    context 'when fetching all the rents' do
-      let(:rents) { create_list(:rent, 3, start_date: Time.zone.today, end_date: Date.tomorrow) }
+    subject(:http_request) { get :index, params: { user_id: user.id } }
 
-      before do
-        get :index
-      end
+    context 'when fetching all the rents' do
+      let(:book) { create(:book) }
+      let(:rents) { create_list(:rent, 3, user_id: user.id, book_id: book.id, start_date: Time.zone.today, end_date: Date.tomorrow) }
 
       it 'reponses with rents json' do
         expected = ActiveModel::Serializer::CollectionSerializer.new(
           rents, each_serializer: RentSerializer
         ).to_json
-        expect(response.body.to_json) =~ JSON.parse(expected)
+        expect(http_request.body.to_json) =~ JSON.parse(expected)
       end
 
       it 'responds with 200 status' do
-        expect(response).to have_http_status(:ok)
+        expect(http_request).to have_http_status(:ok)
       end
     end
   end
 
   describe 'POST #create' do
-    context 'when the request is valid' do
-      let(:book) { create(:book) }
-      let(:rent) { FactoryBot.build(:rent) }
-      let(:valid_attributes) { { book_id: book.id, user_id: user.id, start_date: '2019-10-07', end_date: '2019-10-08' } }
+    subject(:http_request) { post :create, params: { user_id: user.id, book_id: book_id, start_date: '2019-10-07', end_date: '2019-10-08' } }
 
-      before do
-        post :create, params: valid_attributes
-      end
+    context 'when request is valid' do
+      let(:book_id) { create(:book).id }
+      let(:rent) { create(:rent, user_id: user.id, book_id: book_id, start_date: '2019-10-07', end_date: '2019-10-08') }
+      let(:valid_attributes) { { user_id: user.id, book_id: book_id, start_date: '2019-10-07', end_date: '2019-10-08' } }
 
       it 'responses with the rent json' do
-        expect(response.body.to_json) =~ RentSerializer.new(
+        expect(http_request.body.to_json) =~ RentSerializer.new(
           rent, root: false
         ).to_json
       end
 
-      it 'returns status code 200' do
-        expect(response).to have_http_status(:ok)
+      it 'responds with 200 status' do
+        expect(http_request).to have_http_status(:ok)
       end
 
-      it 'when create the rent' do
-        rent = Rent.last
+      it 'when create the rent valid book' do
         expect(rent.book_id).to eq(valid_attributes[:book_id])
-        expect(rent.user_id).to  eq(valid_attributes[:user_id])
-        expect(rent.start_date) =~ (valid_attributes[:start_date])
-        expect(rent.end_date) =~ (valid_attributes[:end_date])
+      end
+
+      it 'when create the rent valid user' do
+        expect(rent.user_id).to eq(valid_attributes[:user_id])
+      end
+
+      it 'when create the rent valid start date' do
+        expect(rent.start_date) =~ valid_attributes[:start_date]
+      end
+
+      it 'when create the rent valid end date' do
+        expect(rent.end_date) =~ valid_attributes[:end_date]
       end
     end
 
-    # context "with invalid params" do
-    #   let(:book) { create(:book) }
-    #   #let(:invalid_attributes) { { book_id: book.id, user_id: user.id, start_date: '2019-10-07', end_date: '2019-10-08' } }
-    #   #let(:invalid_attributes) { { book_id: book.id, user_id: user.id, start_date: '2019-10-07', end_date: '2019-10-07' } }
+    context 'when request is invalid' do
+      let(:book_id) { create(:book).id }
 
-    #   before(:each) do
-    #     post :create, params: { book_id: book.id, user_id: user.id, start_date: '2019-10-07'}
-    #   end
+      it 'returns error messages' do
+        expect(http_request.body['error']).to be_present
+      end
 
-    #   it 'returns status code 422' do
-    #     expect(response).to have_http_status(422)
-    #   end
-
-    #   it 'returns a validation failure message' do
-    #     expect(response.body)
-    #       .to match(/Validation failed: End date can't be blank/)
-    #   end
-    # end
+      it 'responds with 422 status' do
+        expect(http_request).to have_http_status(:unprocessable_entity)
+      end
+    end
   end
 end
