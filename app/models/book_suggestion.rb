88c27a48d4588345b231a9c7b87aa384b320559f
Francisco Abalan <francisco.abalan@pjchile.com>
2019-10-29 14:14:19 -0300

    routes validate presence of user relation to book suggestion

--- app/models/book_suggestion.rb
+++ app/models/book_suggestion.rb
@@ -1,6 +1,6 @@
 class BookSuggestion < ApplicationRecord
   # Relations
-  belongs_to :user, optional: true
+  belongs_to :user
 
   # Validations
   validates :author, :title, :link, :publisher, :year, presence: true
