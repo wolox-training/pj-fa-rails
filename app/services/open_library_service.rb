f6931ee8ec7ec2d6fc70a130f435a85b76c1e744
Francisco Abalan <francisco.abalan@pjchile.com>
2019-11-07 09:36:29 -0300

    api Add empty line after guard clause

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -4,8 +4,8 @@ class OpenLibraryService
 
   def self.execute(isbn)
     response = get('/', query: { bibkeys: "ISBN:#{isbn}", jscmd: 'data', format: 'json' })
-
     raise ActiveRecord::RecordNotFound, 'Book not found' if response.parsed_response.empty?
+
     response["ISBN:#{isbn}"].slice('title', 'subtitle', 'number_of_pages', 'authors')
   end
 end
